1579d05284995c534f63c00888f2605ef0677d3d
Maksim <maxus.info.mail@gmail.com>
2021-06-15 12:07:49 +0300

    Execute command now has proper documentation

---
+++ SharpFunction/Commands/Minecraft/Kill.cs
@@ -0,0 +1,10 @@
+namespace SharpFunction.Commands.Minecraft
+{
+    /// <summary>
+    /// Represents kill command. Equal to Minecraft's <code>/kill {entity: EntitySelector}</code>
+    /// </summary>
+    public sealed class Kill
+    {
+        
+    }
+}
